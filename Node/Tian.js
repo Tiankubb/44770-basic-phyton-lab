c0cb81118d972b47c5edb6aa380b9c3f5cc375af
Tiankubb <kktienwat@gmail.com>
2025-08-09 14:20:56 +0700

    js

---
+++ Node/Tian.js
@@ -0,0 +1,35 @@
+// console.log("Tian");
+
+// blockScope
+// var x = 5
+// if (x===5){
+//     var y = 10
+//     console.log("y ="+y);
+// }
+// console.log("y ="+y);
+
+// Arrow Function
+// fullname = (fname,lname) =>{
+//     return fname + lname
+// }
+// console.log(fullname("Tienwat"," J"));
+
+// Object
+// const cname = "Tienwat"
+// const age = 15
+// const address = "Chiang Mai"
+// const customer = {
+//     cname : cname,
+//     age : age,
+//     address : address,
+//     showData(){
+//         console.log("Name : "+cname)
+//     }
+// }
+// console.log(customer);
+// customer.showData()
+
+// Spread Operator
+// const arraynew = [10, 20, 30]
+// const arraydata = [40, 50,...arraynew]
+// console.log(arraynew)
